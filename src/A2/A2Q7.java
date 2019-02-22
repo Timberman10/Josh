4a3ee88bcfb5c9a1f238f826794689e0d855f8ff
vuksj2164 <vuksj2164@HRH-IC0037417.wrdsb.ca>
2019-02-22 09:00:09 -0500

    Working on A2

---
+++ src/A2/A2Q7.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package A2;
+
+/**
+ *
+ * @author vuksj2164
+ */
+public class A2Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
